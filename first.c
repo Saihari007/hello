49c0bd7e5c59a5decef604a413deb3ba3c81004c
Saihari007 <saihari.andra@gmail.com>
2020-08-07 13:30:02 +0000

    Initial Commit

---
+++ first.c
@@ -0,0 +1,15 @@
+/*******
+* Author :  Sai theja A
+* Date   :   07-08-2020
+* Course : Intro to CS50 IDE
+* About  :  Simple hello world program
+*********/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main () {
+    char* whatsYourLove = "C programmaing";
+    printf("You are starting to love \n \a \a :%s", whatsYourLove);
+    return 0;
+}
