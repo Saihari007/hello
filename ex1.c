39498c1e58d8d511ee2015b24dd3a107c8d04c96
Saihari007 <saihari.andra@gmail.com>
2020-08-07 20:33:56 +0530

    initial commit from my ubuntu machine

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* This is a comment dude */
+int main (int argc, char *argv[]) {
+	printf("Hello Idiots, please fucking pay \a\a ATTENTION \n");
+	printf ("Here, we are trying to declare and print some variables about location metrix\n");
+	printf ("\a Miles and Kms are the keys \n");
+	int distance = 100;
+	int kms = 5 * (distance * 1.64);
+	printf("\n\n \t \t 2 decades later \n\n");
+	/* print the distance you are away from */
+	printf("Where the fuck am I . . .\n");
+	printf("You are actually %d miles  or %d kms away\n", 5*distance, kms);
+	return 0;
+}
